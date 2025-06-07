c307f09c4ec5bbf57c4c472c033846e50e49962f
Your Name <hector.garcia0827@gmail.com>
2025-06-07 19:36:13 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-      ["module-resolver", {
-        root: ["."],
-        alias: {
-          "@components": "./src/components",
-          "@screens": "./src/screens",
-          "@services": "./src/services",
-          "@store": "./src/store",
-          "@navigation": "./src/navigation",
-          "@utils": "./src/utils",
-          "@types": "./src/types",
-          "@config": "./src/config"
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }],
-      "nativewind/babel",
-      "react-native-reanimated/plugin",
-    ],
+      'react-native-reanimated/plugin'
+    ]
   };
 };
