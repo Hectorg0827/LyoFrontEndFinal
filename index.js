c3d064af350004534f4330f8e0d59112b79e613f
Hectorg0827 <hector.garcia0827@gmail.com>
2025-06-08 00:55:13 -0400

    fix: remove duplicate root registration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from "expo";
 import { AppRegistry } from 'react-native';
 
-import App from "./App";
+import App from "./src/App";
 
 // Explicitly register the main component
 AppRegistry.registerComponent('main', () => App);
@@ -9,5 +9,4 @@ AppRegistry.registerComponent('main', () => App);
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
 registerRootComponent(App);
